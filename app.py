dc92463f54163a5c7ec91c86efff80a8c94faa91
JuloEco <jules.ecorcheville@gmail.com>
2026-08-31 21:34:29 +0200

    Load environment variables using dotenv

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, join_room, emit
 from gevent import monkey
 monkey.patch_all()
+from dotenv import load_dotenv()
+load_dotenv()
 
 # ============================================================================
 # CONFIGURATION
